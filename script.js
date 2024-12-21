992aa465995d32bce781575e9932235f7b26be7a
Algaray02 <alfin.rozzaq.270206@gmail.com>
2024-12-21 23:02:40 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const navLinks = document.querySelector(".nav-links");
+function onToggleMenu(e) {
+  e.name = e.name === "menu" ? "close" : "menu";
+  navLinks.classList.toggle("top-0");
+}
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+document.querySelectorAll(".fade-in-up").forEach((el) => observer.observe(el));
